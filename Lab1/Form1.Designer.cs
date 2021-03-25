03bf79508c810d987f597c707a2da496d47d4a49
ViktorKarpilov <wktorion@gmail.com>
2021-03-25 09:59:06 +0200

    Initial commit

---
+++ Lab1/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿
+namespace Lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LoadButton = new System.Windows.Forms.Button();
+            this.AsyncFlag = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.OutPutFolder = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // LoadButton
+            // 
+            this.LoadButton.Location = new System.Drawing.Point(629, 115);
+            this.LoadButton.Name = "LoadButton";
+            this.LoadButton.Size = new System.Drawing.Size(94, 29);
+            this.LoadButton.TabIndex = 0;
+            this.LoadButton.Text = "Load";
+            this.LoadButton.UseVisualStyleBackColor = true;
+            this.LoadButton.Click += new System.EventHandler(this.LoadButton_Click);
+            // 
+            // AsyncFlag
+            // 
+            this.AsyncFlag.AutoSize = true;
+            this.AsyncFlag.Checked = true;
+            this.AsyncFlag.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.AsyncFlag.Location = new System.Drawing.Point(50, 120);
+            this.AsyncFlag.Name = "AsyncFlag";
+            this.AsyncFlag.Size = new System.Drawing.Size(69, 24);
+            this.AsyncFlag.TabIndex = 1;
+            this.AsyncFlag.Text = "Async";
+            this.AsyncFlag.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(186, 117);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(418, 27);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(186, 151);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(418, 27);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(186, 185);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(418, 27);
+            this.textBox3.TabIndex = 4;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // OutPutFolder
+            // 
+            this.OutPutFolder.Location = new System.Drawing.Point(186, 350);
+            this.OutPutFolder.Name = "OutPutFolder";
+            this.OutPutFolder.Size = new System.Drawing.Size(418, 27);
+            this.OutPutFolder.TabIndex = 5;
+            this.OutPutFolder.Text = "./";
+            this.OutPutFolder.TextChanged += new System.EventHandler(this.OutPutFolder_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(338, 314);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Output directory";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.OutPutFolder);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.AsyncFlag);
+            this.Controls.Add(this.LoadButton);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button LoadButton;
+        private System.Windows.Forms.CheckBox AsyncFlag;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox OutPutFolder;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Lab1/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadButton_Click(object sender, EventArgs e)
+        {
+            bool isAllFinded = false;
+            int i = 1;
+            List<string> urls = new();
+
+            while (!isAllFinded)
+            {
+                var elements = this.Controls.Find($"textBox{i}", false);
+                isAllFinded = elements.Length == 0;
+                if (!isAllFinded)
+                    urls.Add(elements.First().Text);
+                i++;
+            }
+
+            Dictionary<string,bool> threadsState = new();
+            Dictionary<Thread,string> threads = new();
+            
+            foreach(string url in urls)
+            {
+                Thread thread = new(() =>
+                {
+                    string html = string.Empty;
+                    HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                    request.AutomaticDecompression = DecompressionMethods.GZip;
+
+                    using HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                    using Stream stream = response.GetResponseStream();
+                    using StreamReader reader = new(stream);
+                    html = reader.ReadToEnd();
+
+                    using StreamWriter file = new(Path.Combine(OutPutFolder.Text + url.Replace("http://", "")));
+                    file.Write(html);
+                    threadsState[url] = true;
+                });
+
+                threadsState.Add(url,false);
+                threads.Add(thread,url);
+            }
+            
+            foreach(var thread in threads)
+            {
+                thread.Key.Start();
+                if (!AsyncFlag.Checked)
+                    while (!threadsState[thread.Value])
+                        Thread.Sleep(10);
+            }
+
+            bool IsDone = false;
+            while (!IsDone)
+            {
+                IsDone = !threadsState.Any(state => state.Value == false);
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OutPutFolder_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
